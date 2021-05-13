f84256e81216e169b5440a24f4b669fff45202cc
Kevin Oliveira <me@kevinoliveira.com.br>
2021-05-13 01:18:27 -0300

    feat(lists): Montagem da lista de álbuns

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SidebarModule } from 'src/app/components/sidebar/sidebar.module';
+import { ListAlbumsModule } from 'src/app/components/lists/list-albums/list-albums.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, SidebarModule],
+  imports: [CommonModule, HomeRoutingModule, SidebarModule, ListAlbumsModule],
 })
 export class HomeModule {}
