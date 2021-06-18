c736006c0c5e0d5019372c431a9dd6add9f8d7e2
Kevin Oliveira <me@kevinoliveira.com.br>
2021-06-18 01:06:47 -0300

    feat(filters): Montagem do filtro de gêneros

--- src/app/components/filters/genre-filter/genre-filter.component.ts
+++ src/app/components/filters/genre-filter/genre-filter.component.ts
@@ -3,13 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-genre-filter',
   templateUrl: './genre-filter.component.html',
-  styleUrls: ['./genre-filter.component.scss']
+  styleUrls: ['./genre-filter.component.scss'],
 })
 export class GenreFilterComponent implements OnInit {
+  optionsReleased: any = [
+    {
+      label: 'Mais recentes',
+      value: 'desc',
+    },
+    {
+      label: 'Mais antigos',
+      value: 'asc',
+    },
+  ];
+  optionsTitle: any = [
+    {
+      label: 'Nome de A-z',
+      value: 'desc',
+    },
+    {
+      label: 'Nome de Z-a',
+      value: 'asc',
+    },
+  ];
+  optionsCountries: any = [
+    {
+      label: 'Alemanha',
+      value: 'alemanha',
+    },
+    {
+      label: 'Brasil',
+      value: 'brasil',
+    },
+  ];
+  orderReleased: string = 'desc';
+  orderTitle: string = 'desc';
+  orderCountries: any = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/filters/genre-filter/genre-filter.module.ts
+++ src/app/components/filters/genre-filter/genre-filter.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GenreFilterComponent } from './genre-filter.component';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [GenreFilterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, NgSelectModule],
   exports: [GenreFilterComponent],
 })
 export class GenreFilterModule {}
