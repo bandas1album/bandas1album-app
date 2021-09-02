a575ed6a5f85ec295feb2383ce71f4eebbe3543d
Kevin Oliveira <me@kevinoliveira.com.br>
2021-09-02 20:53:43 -0300

    fix(album): Limpando imports não utilizados

--- src/app/pages/album/album.module.ts
+++ src/app/pages/album/album.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { AlbumRoutingModule } from './album-routing.module';
 import { AlbumComponent } from './album.component';
-import { DisqusModule } from 'ngx-disqus';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ListLinksModule } from 'src/app/components/lists/list-links/list-links.module';
 import { SidebarModule } from 'src/app/components/sidebar/sidebar.module';
@@ -18,7 +17,6 @@ import { HtmlDecodeModule } from 'src/app/pipes/html-decode/html-decode.module';
   imports: [
     CommonModule,
     AlbumRoutingModule,
-    DisqusModule.forRoot('bandas1album'),
     MatDialogModule,
     ListLinksModule,
     SidebarModule,
