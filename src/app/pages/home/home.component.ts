457b66ed2354cf64b95cbf51353236e9e5176247
Kevin Oliveira <me@kevinoliveira.com.br>
2021-09-09 21:23:27 -0300

    Atualizando seo da home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,38 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import {Apollo, gql} from 'apollo-angular';
+import { SeoService } from 'src/app/services/seo/seo.service';
+
+const query = gql`
+{
+  page(id: "/", idType: URI) {
+    seo {
+      title
+      metaDesc
+    }
+  }
+  albums(first: 95) {
+    edges {
+      node {
+        slug
+        title
+        link
+        acf {
+          artist
+        }
+        featuredImage {
+          node {
+            sourceUrl(size: THUMBNAIL)
+          }
+        }
+      }
+    }
+    pageInfo {
+      total
+    }
+  }
+}
+`;
 
 @Component({
   selector: 'app-home',
@@ -16,6 +48,7 @@ export class HomeComponent implements OnInit {
   constructor(
     @Inject(PLATFORM_ID) platformId: object,
     private apollo: Apollo,
+    private seoService: SeoService
   ) {
     this.isBrowser = isPlatformBrowser(platformId);
   }
@@ -30,38 +63,11 @@ export class HomeComponent implements OnInit {
     }
 
     this.apollo.watchQuery({
-      query: gql`
-        {
-          page(id: "/", idType: URI) {
-            seo {
-              fullHead
-            }
-          }
-          albums(first: 95) {
-            edges {
-              node {
-                slug
-                title
-                link
-                acf {
-                  artist
-                }
-                featuredImage {
-                  node {
-                    sourceUrl(size: THUMBNAIL)
-                  }
-                }
-              }
-            },
-            pageInfo {
-              total
-            }
-          }
-        }
-      `
+      query
     }).valueChanges.subscribe((result: any) => {
       const seo = result.data.page.seo;
-      document.querySelector('head')?.insertAdjacentHTML('beforeend', seo.fullHead);
+      this.seoService.updateTitle(seo.title);
+      this.seoService.updateTag('description', seo.metaDesc);
       this.items = result.data.albums.edges;
       this.total = result.data.albums.pageInfo.total;
       this.loading = false;
--- src/app/services/seo/seo.service.ts
+++ src/app/services/seo/seo.service.ts
@@ -13,13 +13,51 @@ export class SeoService {
     private title: Title
   ) {}
 
-  updateTitle(title: string) {
+  update(meta: any) {
+    const title = `${meta.title} | HSA`;
+    const description = meta.description ? meta.description : '';
+    const image = meta?.image || meta?.images?.desktop;
+
     this.title.setTitle(title);
+    this.meta.updateTag({ name: 'description', content: description });
+
+    this.meta.updateTag({ property: 'og:title', content: title });
+    this.meta.updateTag({ property: 'og:description', content: description });
+
+    this.meta.updateTag({ property: 'twitter:title', content: title });
+    this.meta.updateTag({ name: 'twitter:description', content: description });
+
+    if (image) {
+      this.meta.updateTag({ property: 'og:image', content: image });
+      this.meta.updateTag({ property: 'twitter:image:src', content: image });
+    }
   }
 
-  metatags(data: any) {
-    data.forEach((meta: any) => {
-      this.meta.updateTag(meta);
+  updateOgUrl(url?: string) {
+    if (!url) {
+      url = this.doc.URL;
+    }
+
+    url = url || '';
+
+    this.meta.addTags([
+      { name: 'og:url', content: url },
+      { name: 'twitter:urltwitter:url', content: url }
+    ]);
+
+    this.meta.updateTag({
+      name: 'robots',
+      content: environment.production ? 'index, follow' : 'noindex, nofollow',
     });
+
+    this.createLinkForCanonicalURL();
+  }
+
+  createLinkForCanonicalURL() {
+    const link: HTMLLinkElement = this.doc.createElement('link');
+
+    link.setAttribute('rel', 'canonical');
+    this.doc.head.appendChild(link);
+    link.setAttribute('href', this.doc.URL);
   }
 }
