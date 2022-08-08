85f9569c864093f98945436b5840910cb0cdce10
Kevin Oliveira <me@kevinoliveira.com.br>
2022-08-07 23:40:49 -0300

    Refactor: Removendo imports e ajustando variáveis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: 'enabled',
+      initialNavigation: 'enabledBlocking',
     }),
   ],
   exports: [RouterModule],
