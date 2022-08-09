65b9a7a5eb64c8e65a0f250f5293122ff22f79b0
Kevin Oliveira <me@kevinoliveira.com.br>
2022-08-09 00:37:35 -0300

    Fix: (modules) Ajustando visual das tabs

--- src/app/components/tabs/tabs.module.ts
+++ src/app/components/tabs/tabs.module.ts
@@ -4,10 +4,17 @@ import { LogoModule } from './../logo/logo.module';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabsComponent } from './tabs.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [TabsComponent],
-  imports: [CommonModule, LogoModule, TabsMenuModule, TabsSearchModule],
+  imports: [
+    CommonModule,
+    LogoModule,
+    TabsMenuModule,
+    TabsSearchModule,
+    RouterModule,
+  ],
   exports: [TabsComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
