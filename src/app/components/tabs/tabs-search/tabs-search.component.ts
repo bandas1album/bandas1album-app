c675e1e1758b3d1999e6d0bc5ce64de93e456036
Kevin Oliveira <me@kevinoliveira.com.br>
2022-08-15 02:06:13 -0300

    Feat: (modules) Integrando autocomplete no tabs search

--- src/app/components/tabs/tabs-search/tabs-search.component.ts
+++ src/app/components/tabs/tabs-search/tabs-search.component.ts
@@ -1,15 +1,39 @@
+import { Apollo, gql } from 'apollo-angular';
 import { Component, OnInit } from '@angular/core';
 
+const GET_AUTOCOMPLETE = gql`
+  query GetAutocomplete($search: String) {
+    albums(filters: { title: { startsWith: $search } }) {
+      data {
+        attributes {
+          title
+        }
+      }
+    }
+  }
+`;
 @Component({
   selector: 'app-tabs-search',
   templateUrl: './tabs-search.component.html',
-  styleUrls: ['./tabs-search.component.scss']
+  styleUrls: ['./tabs-search.component.scss'],
 })
 export class TabsSearchComponent implements OnInit {
+  autocompleteList: any = [];
 
-  constructor() { }
+  constructor(private apollo: Apollo) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get(event: any) {
+    this.apollo
+      .watchQuery<any>({
+        query: GET_AUTOCOMPLETE,
+        variables: {
+          search: event.target.value,
+        },
+      })
+      .valueChanges.subscribe(({ data }) => {
+        this.autocompleteList = data.albums.data.map((item: any) => item);
+      });
   }
-
 }
