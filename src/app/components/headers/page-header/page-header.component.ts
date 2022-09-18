7fdfa9f531a2f152d183bfcb4fc209b443509b1a
Kevin Oliveira <me@kevinoliveira.com.br>
2022-09-18 00:04:34 -0300

    Feat: (modules) Ajustando page header fixo

--- src/app/components/headers/page-header/page-header.component.ts
+++ src/app/components/headers/page-header/page-header.component.ts
@@ -1,16 +1,27 @@
 import { Location } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.scss'],
 })
-export class PageHeaderComponent {
+export class PageHeaderComponent implements OnInit {
   @Input() isFixed: boolean = false;
+
   constructor(private location: Location) {}
 
-  back() {
+  ngOnInit(): void {
+    this.headerFixed();
+  }
+
+  public back(): void {
     this.location.back();
+  }
+
+  public headerFixed(): void {
+    window.addEventListener('scroll', () => {
+      this.isFixed = window.scrollY >= 80 ? true : false;
+    });
   }
 }
