a595da09daf38594bdec286a77c4130d17dbdc87
Kevin Oliveira <me@kevinoliveira.com.br>
2023-06-08 00:32:42 -0300

    Add RTL config and first test

--- src/components/CardAlbum/test.tsx
+++ src/components/CardAlbum/test.tsx
@@ -6,7 +6,7 @@ describe('<CardAlbum />', () => {
   it('should render the heading', () => {
     const { container } = render(<CardAlbum />)
 
-    expect(screen.getByRole('heading', { name: /teste/i })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /test/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
