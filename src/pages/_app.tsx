043ab303b138db7347781e70f58b3c1b030b28a7
Kevin Oliveira <me@kevinoliveira.com.br>
2023-06-12 12:54:15 -0300

    Feat: Install next progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import GlobalStyles from '@/styles/global'
+import NextNProgress from 'nextjs-progressbar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,6 +16,14 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <GlobalStyles />
       <main>
+        <NextNProgress
+          color="#a58a67"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
+
         <Component {...pageProps} />
       </main>
     </>
