6beb2228c7783faabb4756888e6a4749b6ebe62f
Kevin Oliveira <me@kevinoliveira.com.br>
2023-06-13 23:32:00 -0300

    Feat: (pages) Adicionando seo next

---
+++ next-seo.config.js
@@ -0,0 +1,29 @@
+export default {
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: '/favicon.ico'
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/logo.png',
+      sizes: '76x76'
+    },
+    {
+      rel: 'manifest',
+      href: '/manifest.json'
+    }
+  ],
+  canonical: 'https://bandas1album.com.br/',
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://bandas1album.com.br',
+    siteName: 'Bandas de 1 Álbum'
+  },
+  twitter: {
+    handle: '@bandas1album',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,6 +1,7 @@
 import { AlbumProps } from '@/types'
 import ListAlbums from '@/components/ListAlbums'
 import Head from 'next/head'
+import { NextSeo } from 'next-seo'
 
 type HomeTemplateProps = {
   albums: AlbumProps[]
@@ -12,6 +13,21 @@ export default function HomeTemplate({ albums }: HomeTemplateProps) {
       <Head>
         <title>Bandas de 1 Álbum</title>
       </Head>
+      <NextSeo
+        title="Bandas de 1 Álbum"
+        description="Eternizando bandas e artistas que lançaram apenas um álbum."
+        openGraph={{
+          url: 'https://bandas1album.com.br/',
+          images: [
+            {
+              url: '/cover.png',
+              width: 1280,
+              height: 720,
+              alt: 'Bandas de 1 Álbum'
+            }
+          ]
+        }}
+      />
       <ListAlbums albums={albums} />
     </>
   )
