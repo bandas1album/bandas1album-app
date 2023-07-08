0bb191fcb4d5319a51443573da4b11799685fc4a
Kevin Oliveira <me@kevinoliveira.com.br>
2023-07-08 01:49:30 -0300

    Fix: (core) Add eslint-disable to graphql files

--- src/graphql/generated/fragment-masking.ts
+++ src/graphql/generated/fragment-masking.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { ResultOf, DocumentTypeDecoration, TypedDocumentNode } from '@graphql-typed-document-node/core';
 import { FragmentDefinitionNode } from 'graphql';
 import { Incremental } from './graphql';
