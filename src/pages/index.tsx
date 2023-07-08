2e2203288cfd9e8fbcb2837aee2411b36ad573ad
Kevin Oliveira <me@kevinoliveira.com.br>
2023-07-08 17:18:26 -0300

    Refactor: (pages) Change static props to server props on first loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import client from '@/graphql/client'
 import { AlbumConnection, GetAlbumsQuery } from '@/graphql/generated/graphql'
 import { GET_ALBUMS } from '@/graphql/queries'
 import HomeTemplate from '@/templates/Home'
-import { GetStaticProps } from 'next'
+import { GetServerSideProps } from 'next'
 
 export default function Home({ nodes, pageInfo, edges }: AlbumConnection) {
   return <HomeTemplate nodes={nodes} pageInfo={pageInfo} edges={edges} />
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const { data } = await client.query({
     query: GET_ALBUMS,
     variables: {
