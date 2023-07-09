cab8966ecd929cf5b9ed331129962fcd9a5e169f
Kevin Oliveira <me@kevinoliveira.com.br>
2023-07-08 21:34:20 -0300

    Feat: (core) Add analytics gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import StyledComponentsRegistry from '../../lib/registry'
+import Script from 'next/script'
 
 export default function Document({ children }: { children: React.ReactNode }) {
   return (
@@ -17,6 +18,16 @@ export default function Document({ children }: { children: React.ReactNode }) {
         />
       </Head>
       <body data-theme="dark">
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-XYCDCKWDJ2" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-XYCDCKWDJ2');
+        `}
+        </Script>
         <Main />
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         <NextScript />
