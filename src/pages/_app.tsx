40bff15d59a86a5d55bb69b4b87fea4e3e6e8bf5
Kevin Oliveira <me@kevinoliveira.com.br>
2023-07-08 23:57:39 -0300

    Fix: (core) Fix gtag events

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,24 @@ import NextNProgress from 'nextjs-progressbar'
 import SEO from '../../next-seo.config'
 import { DefaultSeo } from 'next-seo'
 import Tabs from '@/components/Tabs'
-import Script from 'next/script'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
+import Script from 'next/script'
+import * as gtag from '../../gtag'
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
+
   useEffect(() => {
     router.prefetch = async () => void {}
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
   }, [router])
   return (
     <>
@@ -22,16 +32,24 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#292556" />
       </Head>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-XYCDCKWDJ2" />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
- 
-          gtag('config', 'G-XYCDCKWDJ2');
-        `}
-      </Script>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
       <DefaultSeo {...SEO} />
       <GlobalStyles />
       <main role="main">
